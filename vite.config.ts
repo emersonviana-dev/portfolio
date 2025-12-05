3b707b2b7d36289853869a7fd0752e995e296edd
Emerson Viana | Developer <emerson.emtec@gmail.com>
2025-12-04 23:38:07 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio/", // 🔑 coloque aqui o nome exato do repositório
   server: {
     host: "::",
     port: 8080,
